b77583d2f99242d52763240c017d6fefd6553844
Maciej Sznurawa <msznurawa@gmail.com>
2020-01-21 11:20:47 +0100

    Check server connection

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,24 @@ import React from 'react';
 
 class App extends React.Component {
 
+  state = {
+    posts: [],
+  }
+
+  // connection to server endpoint, endpoint answer is written to this.state.posts
+  componentDidMount() {
+    fetch('http://localhost:8000/api/posts')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({posts: res});
+    });
+  }
+
   render() {
     return (
-      <div>Hello world!</div>
+      <div>
+        <ul>{this.state.posts.map(post => <li key={post.id}>{post.title}</li>)}</ul>
+      </div>
     );
   }
 };
