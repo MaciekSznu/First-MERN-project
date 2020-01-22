c8484f918c2457106d7bb9e7af03a7762bdd9f74
Maciej Sznurawa <msznurawa@gmail.com>
2020-01-22 14:35:56 +0100

    Add SinglePost functionality

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Posts from './components/pages/Posts/PostsPage';
 import Contact from './components/pages/Contact/ContactPage';
 import NotFound from './components/pages/NotFound/NotFoundPage';
 import AddPost from './components/pages/AddPostPage/AddPostPage';
+import SinglePost from './components/pages/SinglePostPage/SinglePostPage';
+
 
 
 
@@ -21,6 +23,7 @@ class App extends React.Component {
          <Route path='/posts' exact component={Posts} />
          <Route path='/contact' exact component={Contact} />
          <Route path='/posts/new' exact component={AddPost} />
+         <Route path='/posts/:id' exact component={SinglePost}/>
          <Route component={NotFound} />
        </Switch>
      </MainLayout>
--- client/src/components/pages/Posts/PostsPage.js
+++ client/src/components/pages/Posts/PostsPage.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const PostsPage = () => (
-  <div>
-    <h1>Posts</h1>
-  </div>
-);
+class PostsPage extends React.Component {
+
+  state = {
+    posts: [],
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8000/api/posts')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ posts: res });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Posts</h1>
+        <ul>
+            {this.state.posts.map(post => <Link to={`/posts/${post.id}`}><li key={post.id}>{post.title}</li></Link>)}
+        </ul>
+      </div>
+    );
+  }
+
+};
 
 export default PostsPage;
