6b8587f285b92b30d75dcce564faa6739f6b0466
Maciej Sznurawa <msznurawa@gmail.com>
2020-01-29 17:23:15 +0100

    Clear console.logs

--- client/src/components/features/PostsCounter/PostsCounter.js
+++ client/src/components/features/PostsCounter/PostsCounter.js
@@ -4,7 +4,6 @@ class PostsCounter extends React.Component {
 
   render() {
     const { amount } = this.props;
-    console.log(this.props);
     const amountInfo = amount > 0 ? amount : '- no posts -';
     return (
       <div>Posts amount: {amountInfo} </div>
