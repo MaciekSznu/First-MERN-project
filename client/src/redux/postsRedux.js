81fb7fed3dec9cdf875b7b8d53ac7f59ea42db20
Maciej Sznurawa <msznurawa@gmail.com>
2020-02-04 09:00:03 +0100

    Add SinglePost selector, action, thunk & reducer

--- client/src/redux/postsRedux.js
+++ client/src/redux/postsRedux.js
@@ -5,6 +5,7 @@ import { API_URL } from '../config';
 export const getPosts = ({posts}) => posts.data;
 export const getPostsAmount = ({posts}) => posts.data.length;
 export const getRequest = ({posts}) => posts.request;
+export const getSinglePost = ({posts}) => posts.singlePost;
 
 
 // ACTIONS & CREATORS
@@ -16,16 +17,19 @@ export const LOAD_POSTS = createActionName('LOAD_POSTS');
 export const START_REQUEST = createActionName('START_REQUEST');
 export const END_REQUEST = createActionName('END_REQUEST');
 export const ERROR_REQUEST = createActionName('ERROR_REQUEST');
+export const LOAD_SINGLE_POST = createActionName('LOAD_SINGLE_POST');
 
 
 export const loadPosts = payload => ({ payload, type: LOAD_POSTS });
 export const startRequest = () => ({type: START_REQUEST});
 export const endRequest = () => ({type: END_REQUEST});
 export const errorRequest = error => ({ error, type: ERROR_REQUEST });
+export const loadSinglePostRequest = () => ({ payload, type: LOAD_SINGLE_POST });
 
 // INITIAL STATE
 const initialState = {
   data: [],
+  singlePost: null,
   request: {
     pending: false,
     error: null,
@@ -53,12 +57,34 @@ export const loadPostsRequest = () => {
   };
 };
 
+export const loadSinglePostRequest = (id) => {
+  return async dispatch => {
+
+    dispatch(startRequest());
+    try {
+
+      let res = await axios.get(`${API_URL}/posts/${id}`);
+      await new Promise((resolve, reject) => setTimeout(resolve, 2000));
+      dispatch(loadSinglePost(res.data));
+      dispatch(endRequest());
+    
+    } catch(e) {
+      dispatch(errorRequest(e.message));
+    }
+
+  };
+};
+
+
+
 // REDUCER
 export default function reducer(statePart = initialState, action = {}) {
   switch (action.type) {
     case LOAD_POSTS:
       // new object with all statePart atributes and additional atribute data
       return { ...statePart, data: action.payload };
+    case LOAD_SINGLE_POST:
+      return { ...statePart, singlePost: action.payload};
     case START_REQUEST:
       // changing request.pending to true
       return { ...statePart, request: { pending: true, error: null, succes: null } };
