fddc80465173665bfc128e3bdc2d32be6d09e578
Maciej Sznurawa <msznurawa@gmail.com>
2020-02-07 11:54:00 +0100

    Add addPost endpoint, add uuid library

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post.model');
+const uuid = require('uuid');
 
 // get all posts
 // first endpoint
@@ -14,6 +15,7 @@ exports.getPosts = async (req, res) => {
   // Znajdź wszystkie posty i zwróć je do klienta wraz z kodem 200 (sukces), o ile udało się wykonać funkcję. Jeśli pojawił się błąd, to zamiast tego, zwróć go, wraz z kodem 500.
 };
 
+// get single post
 exports.getSinglePost = async (req, res) => {
 
   try {
@@ -23,3 +25,26 @@ exports.getSinglePost = async (req, res) => {
     res.status(500).json(err);
   }
 };
+
+// add new post
+
+exports.addPost = async (req, res) => {
+
+  try {
+    const { title, author, content } = req.body;
+
+    let newPost = new Post(req.body);
+    //let newPost = new Post();
+    // newPost.title = title;
+    // newPost.author = author;
+    // newPost.content = content;
+    newPost.id = uuid();
+
+    postSaved = await newPost.save();
+    res.status(200).json(postSaved);
+
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
+}
