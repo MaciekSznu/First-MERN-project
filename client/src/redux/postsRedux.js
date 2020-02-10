0a5bf35bfd75fee0db88c411dbe020cb60d8670f
Maciej Sznurawa <msznurawa@gmail.com>
2020-02-10 20:55:46 +0100

    Add selectors, action creators, reducer and thunk

--- client/src/redux/postsRedux.js
+++ client/src/redux/postsRedux.js
@@ -6,7 +6,7 @@ export const getPosts = ({posts}) => posts.data;
 export const getPostsAmount = ({posts}) => posts.data.length;
 export const getRequest = ({posts}) => posts.request;
 export const getSinglePost = ({posts}) => posts.singlePost;
-
+export const getPages = ({ posts }) => Math.ceil(posts.amount / posts.postsPerPage);
 
 // ACTIONS & CREATORS
 //action name creator
@@ -19,7 +19,7 @@ export const END_REQUEST = createActionName('END_REQUEST');
 export const ERROR_REQUEST = createActionName('ERROR_REQUEST');
 export const LOAD_SINGLE_POST = createActionName('LOAD_SINGLE_POST');
 export const RESET_REQUEST = createActionName('RESET_REQUEST');
-
+export const LOAD_POSTS_PAGE = createActionName('LOAD_POSTS_PAGE');
 
 export const loadPosts = payload => ({ payload, type: LOAD_POSTS });
 export const startRequest = () => ({type: START_REQUEST});
@@ -27,6 +27,7 @@ export const endRequest = () => ({type: END_REQUEST});
 export const errorRequest = error => ({ error, type: ERROR_REQUEST });
 export const loadSinglePost = payload => ({ payload, type: LOAD_SINGLE_POST });
 export const resetRequest = () => ({type: RESET_REQUEST});
+export const loadPostsByPage = payload => ({ payload, type: LOAD_POSTS_PAGE });
 
 // INITIAL STATE
 const initialState = {
@@ -37,6 +38,9 @@ const initialState = {
     error: null,
     success: null,
   },
+  amount: 0,
+  postsPerPage: 10,
+  presentPage: 1,
 }
 
 // THUNKS
@@ -94,7 +98,36 @@ export const addPostRequest = (post) => {
   };
 };
 
+export const loadPostsByPageRequest = (page) => {
+  return async dispatch => {
 
+    dispatch(startRequest());
+    try {
+
+      const postsPerPage = 10;
+
+      const startAt = (page - 1) * postsPerPage;
+      const limit = postsPerPage;
+
+      let res = await axios.get(`${API_URL}/posts/range/${startAt}/${limit}`);
+      await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+
+      const payload = {
+        posts: res.data.posts,
+        amount: res.data.amount,
+        postsPerPage,
+        presentPage: page,
+      };
+
+      dispatch(loadPostsByPage(payload));
+      dispatch(endRequest());
+    }
+    catch(e) {
+      dispatch(errorRequest(e.message));
+    }
+
+  };
+};
 
 // REDUCER
 export default function reducer(statePart = initialState, action = {}) {
@@ -104,6 +137,15 @@ export default function reducer(statePart = initialState, action = {}) {
       return { ...statePart, data: action.payload };
     case LOAD_SINGLE_POST:
       return { ...statePart, singlePost: action.payload};
+    case LOAD_POSTS_PAGE:
+      // new object with amount and posts from server request; but postsPerPage and presentPage is prepared locally
+      return {
+        ...statePart,
+        postsPerPage: action.payload.postsPerPage,
+        presentPage: action.payload.presentPage,
+        amount: action.payload.amount,
+        data: [...action.payload.posts],
+      };
     case START_REQUEST:
       // changing request.pending to true
       return { ...statePart, request: { pending: true, error: null, succes: null } };
