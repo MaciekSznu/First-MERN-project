eb61741a8653f935f42315428fae435abfed5781
Maciej Sznurawa <msznurawa@gmail.com>
2020-03-05 17:40:58 +0100

    Modify API_URL, add BASE_URL to postRedux

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8000/api';
+export const API_URL = '/api' || 'http://localhost:8000/api';
 export const BASE_URL = 'https://maciek-react-blog.herokuapp.com';
--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   PORT: 8000,
-  DB: 'mongodb+srv://MaciekSznu:x67BHkkF8ZnDuOSQ@cluster0-htgqe.mongodb.net/test?retryWrites=true&w=majority',
+  DB: 'mongodb+srv://MaciekSznu:zT1qplRwxm9PKsxH@cluster0-htgqe.mongodb.net/test?retryWrites=true&w=majority',
 };
