7c245491e9f6fcefaedd36e470910a6959859997
Maciej Sznurawa <msznurawa@gmail.com>
2020-03-05 18:05:24 +0100

    add build

---
+++ client/build/precache-manifest.6d0268a06198ebf086d782fa76af2ae0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2eded047dc715c4643794d09d87f42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b98926f7fe5f82db52af",
+    "url": "/static/css/2.bac525f7.chunk.css"
+  },
+  {
+    "revision": "2838998044f21a0e22e9",
+    "url": "/static/css/main.bd183efb.chunk.css"
+  },
+  {
+    "revision": "b98926f7fe5f82db52af",
+    "url": "/static/js/2.06f7f358.chunk.js"
+  },
+  {
+    "revision": "2e1dd1d06e0456f61713b554d68961f9",
+    "url": "/static/js/2.06f7f358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2838998044f21a0e22e9",
+    "url": "/static/js/main.08ac2ec5.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
