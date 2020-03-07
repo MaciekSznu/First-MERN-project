24405bb9c507fd9872d5b15b37cef0e594091269
Maciej Sznurawa <msznurawa@gmail.com>
2020-03-07 14:17:33 +0100

    change atlas connection, delete build

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  DB: 'mongodb+srv://MaciekSznu:Ei2OLNSXGGhDIz2Q@cluster0-htgqe.mongodb.net/test?retryWrites=true&w=majority',
+  DB: 'mongodb+srv://MaciekSznu:Ei2OLNSXGGhDIz2Q@reactblog-htgqe.mongodb.net/test?retryWrites=true&w=majority',
 };
