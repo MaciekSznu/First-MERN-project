e799185f4b41619a1c958bb5e171ef3f799dedb6
Maciej Sznurawa <msznurawa@gmail.com>
2020-03-07 14:25:51 +0100

    change db acces to env

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  DB: 'mongodb+srv://MaciekSznu:Ei2OLNSXGGhDIz2Q@reactblog-htgqe.mongodb.net/test?retryWrites=true&w=majority',
+  DB: process.env.DB,
 };
